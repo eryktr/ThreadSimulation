0cf9c99b4f48543e147bb0fdc89af12f70911a0a
LaTeX Enthusiast <36778031+LaTeXEnthusiast@users.noreply.github.com>
2018-04-25 13:01:41 +0200

    Gui finished

--- src/simulation/Controller.java
+++ src/simulation/Controller.java
@@ -1,6 +1,21 @@
 package simulation;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+
 public class Controller
 {
+    @FXML
+    private Pane simulationPane;
+    @FXML
+    private TextField mTextField, nTextField, kTextField, pTextField;
+
+    public void startSimulation()
+    {
+
+    }
+
+
 
 }
